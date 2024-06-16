f19da2b2ca46934b39976ff71d1bc2802c74f99c
Sylvan Sam <sylvansam@gmail.com>
2024-06-16 23:58:04 +0530

    refactored  package names

---
+++ src/test/java/au/com/sylvansam/playground/service_discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.sylvansam.playground.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
