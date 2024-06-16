40bf5db5f13ba9a6c169a8185001d07e1cb6221e
Sylvan Sam <sylvansam@gmail.com>
2024-06-16 13:05:08 +0530

    spring initialiser for service discovery

---
+++ src/test/java/au/com/sylvansam/playground/splitwise/service_discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.sylvansam.playground.splitwise.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
